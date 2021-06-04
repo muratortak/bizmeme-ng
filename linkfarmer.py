17b4465cc01952f6bbb5ca64df7453467fbf7f59
Saori Yuko <85325444+saoriyuko@users.noreply.github.com>
2021-06-03 20:15:35 -0700

    Revert "i love people"

--- linkfarmer.py
+++ linkfarmer.py
@@ -1,4 +1,3 @@
-from html import unscape
 from re import search, findall
 from utils.chandata import ChanBoards
 from utils.operations import getThreadIdsFromCatalog, getThread, getCommentsFromThreadAsList
@@ -14,8 +13,21 @@ for counter, threadId in enumerate(threadsIdList):
 
     thread = getThread(board, threadId)
     if thread != None:
-        for comment in getCommentsFromThreadAsList(thread):
-            comment = html.unescape(comment)
+        for c in getCommentsFromThreadAsList(thread):
+            comment = c
+            comment = comment.replace("<br>", "\n")
+            comment = comment.replace("&quot;", "\"")
+            comment = comment.replace("&gt;", ">")
+            comment = comment.replace("&lt;", "<")
+            comment = comment.replace("<wbr>", "")
+            comment = comment.replace("<span class=\"quote\">","")
+            comment = comment.replace("</span>","")
+            comment = comment.replace("&#039;","\'")
+            comment = comment.replace("&amp;","&")
+            comment = comment.replace("</a>","")
+            comment = comment.replace("<a>","")
+            comment = comment.replace("\""," ")
+            comment = comment.replace(">"," ")
 
             t = findall(r'(https?://\S+)', comment)
             if len(t) > 0:
